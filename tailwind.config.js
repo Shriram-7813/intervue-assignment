a8288e4f08200cf78a9bbf46b622099fbb3a32ed
Shriram-7813 <154993074+Shriram-7813@users.noreply.github.com>
2025-07-06 12:03:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/input.css',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['"Sora"', 'sans-serif'],
+      },},
+  },
+  plugins: [],
+}; 
